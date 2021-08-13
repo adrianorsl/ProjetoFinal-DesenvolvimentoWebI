063e93670e71ca10341d10a4f40b4273473aed1c
Adriano Zimmermann <adrianorslsc@hotmail.com>
2021-08-13 17:11:38 -0300

    Update script_bd.sql

--- conf/script_bd.sql
+++ conf/script_bd.sql
@@ -6,3 +6,16 @@ CREATE TABLE `pessoa` (
   `codigo` int(5) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`codigo`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `notas` (
+  `aluno` varchar(45) NOT NULL,
+  `nota` float(4),
+  `func` varchar(11) NOT NULL,
+  `cod` int(5) NOT NULL,
+  FOREIGN KEY (`cod`),
+  REFERENCES `pessoa`(`codigo`),
+  FOREIGN KEY (`func`),
+  REFERENCES `pessoa`(`funcao`),
+  FOREIGN KEY (`aluno`),
+  REFERENCES `pessoa`(`nome`),
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
