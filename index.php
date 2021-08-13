64d0c943b66975dbb3b93fae7d12eb7d37570f44
Adriano Zimmermann <adrianorslsc@hotmail.com>
2021-08-13 17:21:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     require_once "conf/Conexao.php";
     $logo1 = "Img/Logo1.jpg";
     $title = "Centro de Formações Lontrense";
+    $nome = (isset($_POST['nome']) ? $_POST['nome'] : '');
+    $senha = (isset($_POST['senha']) ? $_POST['senha'] : '');
  
 ?>
 <html lang="pt-br">
@@ -14,6 +16,22 @@
     <body>
         <h3> <?php echo "<img src = $logo1  />"; ?> </h3> 
         <h1> <?php echo $title; ?> </h1>
+
+        <?php echo "Informe o nome e senha" ?>
+        <form action="" method="post">
+        <fieldset>
+        <legend>Nome</legend>
+            <label for="nome"></label>
+            <input type="text" name="nome" id="nome" placeholder="Digite seu nome" >
+        <legend>Senha</legend>
+            <label for="senha"></label>
+            <input type="text" name="senha" id="senha" placeholder="Digite sua senha" >
+        </fieldset>
+        <fieldset>
+            <input type="submit" name="ok" id="ok" value="Ok" >
+        </fieldset>
+        </form>
+
        
     </body>
 </html>
