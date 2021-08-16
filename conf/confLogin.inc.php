502b3e0d668078a35594f45a4375f1ebd114d704
Adriano Zimmermann <adrianorslsc@hotmail.com>
2021-08-16 13:32:52 -0300

    Create confLogin.inc.php

---
+++ conf/confLogin.inc.php
@@ -0,0 +1,11 @@
+<?php
+	date_default_timezone_set('America/Sao_Paulo');
+	// Banco de Dados para configuração
+	$url = "127.0.0.1"; // IP do host
+	$dbname="login"; // Nome do database
+	$usuario="root"; // Usuário do database
+	$password=""; // Senha do database
+	
+	// Tabelas do Banco de Dados
+	$tb_login = "login";
+?>
