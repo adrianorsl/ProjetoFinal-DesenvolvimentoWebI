0137990ceeb971c1f0aa37acf9d013bbae7ca519
Adriano Zimmermann <adrianorslsc@hotmail.com>
2021-08-16 13:33:04 -0300

    Update script_bd.sql

--- conf/script_bd.sql
+++ conf/script_bd.sql
@@ -1,21 +1,24 @@
-CREATE TABLE `pessoa` (
-  `nome` varchar(45) NOT NULL,
-  `cpf` varchar(11) NOT NULL,
-  `funcao` varchar(11) NOT NULL,
-  `senha` int(6)  NOT NULL,
-  `codigo` int(5) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`codigo`)
+CREATE DATABASE  IF NOT EXISTS autoEscola /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE autoEscola;
+
+
+CREATE TABLE IF NOT EXISTS pessoa (
+  nome varchar(45) NOT NULL,
+  cpf varchar(11) NOT NULL,
+  funcao varchar(11) NOT NULL,
+  tipo int(1),
+  senha varchar(45)  NOT NULL,
+  codigo int(5) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (codigo),
+  UNIQUE(matricula)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
-CREATE TABLE `notas` (
-  `aluno` varchar(45) NOT NULL,
-  `nota` float(4),
-  `func` varchar(11) NOT NULL,
-  `cod` int(5) NOT NULL,
-  FOREIGN KEY (`cod`)
-  REFERENCES `pessoa`(`codigo`),
-  FOREIGN KEY (`func`)
-  REFERENCES `pessoa`(`funcao`),
-  FOREIGN KEY (`aluno`)
-  REFERENCES `pessoa`(`nome`)
+
+CREATE TABLE IF NOT EXISTS notas (
+  aluno varchar(45) NOT NULL,
+  nota float(4),
+  func varchar(11) NOT NULL,
+  cod int(5) NOT NULL,
+  FOREIGN KEY (aluno)
+  REFERENCES pessoa(nome)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
